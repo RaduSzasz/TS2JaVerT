f1ff9b6032d32196e512795eeebc5c6210d6f14b
Radu Szasz <ras114@ic.ac.uk>
2018-05-24 17:38:21 +0100

    Add subclassing example from report to repo

---
+++ test/testPrograms/SimpleExtendingClasses.ts
@@ -0,0 +1,23 @@
+class Animal {
+    private position: number;
+    constructor(position: number) {
+        this.position = position;
+    }
+    public walk(distance: number): void {
+        this.position = this.position + distance;
+    }
+}
+
+class Cat extends Animal {
+    private name: string;
+    constructor(position: number, name: string) {
+        super(position);
+        this.name = name;
+    }
+    public meow(): void {
+        console.log("Meow!! I’m " + this.name + "!");
+    }
+}
+
+var cat: Cat = new Cat(3, "Bob");
+cat.meow();
